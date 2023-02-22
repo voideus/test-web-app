5cc80f372e188324fd6c6ae91045252b48df39df
voideus <bikash.tamang@wesionary.team>
2023-02-22 10:38:21 +0545

    initial commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path"
+)
+
+var pathToTemplates = "./templates/"
+
+type TemplateData struct {
+	IP   string
+	Data map[string]any
+}
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data *TemplateData) error {
+	// parse template from dist.
+	parsedTemplate, err := template.ParseFiles(path.Join(pathToTemplates, t))
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+
+	data.IP = app.ipFromContext(r.Context())
+
+	// execute template, passing data, if any
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (app *application) Login(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	email := r.Form.Get("email")
+	password := r.Form.Get("password")
+
+	log.Println(email, password)
+
+	fmt.Fprint(w, email)
+}
