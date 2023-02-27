54a577598be7bc0216c7b167c5fd4797625a0647
voideus <bikash.tamang@wesionary.team>
2023-02-27 16:31:17 +0545

    feat: add connection to postgres db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"webapp/pkg/db"
 
 	"github.com/alexedwards/scs/v2"
 )
 
 type application struct {
+	DSN     string
+	DB      db.PostgresConn
 	Session *scs.SessionManager
 }
 
 func main() {
 	// set up an app config
 	app := application{}
+
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5433 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = db.PostgresConn{DB: conn}
 
 	// get a session mananger
 	app.Session = getSession()
@@ -22,7 +36,7 @@ func main() {
 	log.Println("Starting server on port 8080...")
 
 	// start the server
-	err := http.ListenAndServe(":8080", app.routes())
+	err = http.ListenAndServe(":8080", app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
